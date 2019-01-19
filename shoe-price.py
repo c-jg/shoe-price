c560145d454203ee6b97f0558e9d56a034d34f30
Connor Guerrero <43872475+cojguerrero@users.noreply.github.com>
2019-01-18 18:27:55 -0800

    Update shoe-price.py
    Requests used instead of urllib2.

--- shoe-price.py
+++ shoe-price.py
@@ -1,6 +1,5 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-import requests, smtplib, time
+import requests, smtplib
+from bs4 import BeautifulSoup
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from string import Template
@@ -19,31 +18,23 @@ def read_template(filename):
         template_file_content = template_file.read()
     return Template(template_file_content)
 
-
 names, emails = get_contacts('contacts.txt')
 message_template = read_template('body.txt')
- 
-to_file = open('contacts.txt','r')
-to_text = to_file.read().strip()
-to_file.close()
-to = to_text
+email_addr = 'XXXXXXXXX@XXXXXXXXXX.COM'
+# URL of shoes on Nike.com
+url = 'https://www.nike.com/t/zoom-pegasus-turbo-mens-running-shoe-Z163c3/AJ4114-486'
 
-email_addr = 'xxxxxxxxxx@xxxxxxxxxx.com'    
+req = requests.get(url)
+site = req.text
+html = BeautifulSoup(site, "html.parser")
 
-n_url = 'https://www.nike.com/t/epic-react-flyknit-mens-running-shoe-mfm75M/AQ0067-102'
-
-n_site = uReq(n_url)
-n_page = n_site.read()
-n_site.close()
-page_soup = soup(n_page, "html.parser")
-
-price = page_soup.findAll("div",{"class":"ncss-base ta-sm-r fs16-sm"})
+price = html.findAll("div",{"class":"ncss-base ta-sm-r fs16-sm"})
 shoe_price = price[1]
 dollar_amt = price[1].get_text() 
 discount = shoe_price.div.div["class"]
 sale_price = shoe_price.div.div.get_text()
 
-shoe = page_soup.find("h1",{"data-test":"product-title"})
+shoe = html.find("h1",{"data-test":"product-title"})
 shoe_name = shoe.get_text()
 
 if 'mb-1-sm' in discount:
@@ -52,7 +43,8 @@ if 'mb-1-sm' in discount:
         conn = smtplib.SMTP('smtp.gmail.com', 587) # SMTP address and port
         conn.ehlo()
         conn.starttls()
-        conn.login(email_addr, 'xxxxxxxxxxxxxxxxxx')
+        # Enter one-time Google account password below
+        conn.login(email_addr, 'xxxxxxxxxxxxxxxxx')
         msg = MIMEMultipart()
         message = message_template.substitute(CONTACT_NAME=name.title(),SHOE=shoe_name,
         SP=sale_price)
